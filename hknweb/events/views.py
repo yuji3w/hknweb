9ec74a445faa8deae759b780bc9142a03ed941d4
Kevin <chevin_ken@berkeley.edu>
2019-09-17 21:14:43 -0700

    Google calendar

--- hknweb/events/views.py
+++ hknweb/events/views.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.template import loader
@@ -8,6 +9,12 @@ from django.db.models import F # Avoids changing database values without risking
 from .models import Event, Rsvp
 from .forms import EventForm
 from hknweb.models import Profile
+
+#Google calendar
+
+import datetime
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 def index(request):
     events = Event.objects.order_by('-start_time')
@@ -60,7 +67,9 @@ def add_event(request):
     form = EventForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-            form.save()
+            # create_event(form)
+            event = form.save()
+            new_google_calendar(event)
             messages.success(request, 'Event has been added!')
             return redirect('/events')
         else:
@@ -68,3 +77,29 @@ def add_event(request):
             messages.success(request, 'Something went wrong oops')
             return render(request, 'events/add_event.html', {'form': EventForm(None)})
     return render(request, 'events/add_event.html', {'form': EventForm(None)})
+
+def new_google_calendar(event):
+    service = build_service()
+    google_calendar_event = {
+        'summary': "(" + event.event_type.type + ")" + event.name,
+        'location': event.location,
+        'description': event.description,
+        'start': {
+            'dateTime': event.start_time.isoformat(),
+            'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+            'dateTime': event.end_time.isoformat(),
+            'timeZone': 'America/Los_Angeles'
+        }
+    }
+    event = service.events().insert(calendarId= 'primary', body = google_calendar_event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+
+def build_service():
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes = SCOPES)
+    creds = flow.run_console()
+    # creds = flow.run_local_server(port = 0)
+    service = build('calendar', 'v3', credentials = creds)  
+    return service
